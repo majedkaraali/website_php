e155921945d370658ee28bb3e7084195e710bd39
Majed <majedkaraali@gmail.com>
2023-10-24 17:27:06 +0300

    NEW

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merhaba</title>
+</header>
+
+    <?php
+        $first_num=550;
+        $second_num=550;
+
+        if ($first_num>$second_num){
+            echo "The First num is bgreater than the second num";
+        }
+   
+
+        else if ($first_num<$second_num) {
+            echo "The First num is  less than the second num";
+        }
+
+        else
+        {echo "The numbers are equal";}
+    ?>
+
+<body>
+    
+</body>
+
+
+</html>
+
+
