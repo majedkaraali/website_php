1faa9ffb49917f371c2db846d340c932a80b4bc7
Majed <majedkaraali@gmail.com>
2023-11-05 16:01:08 +0300

    update

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <html>
 <body>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
-  SName: <input type="text" name="sfname">
-  <input type="submit">
-</form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // collect value of input field
-  $name = $_POST['fname'];
-  $sname = $_POST['sfname'];
-  if (empty($name)) {
-    echo "Name is empty";
-  } else {
-    echo $name;
-    echo '<br>';
-    echo $sname;
 
-  }
-}
 ?>
 
 </body>
