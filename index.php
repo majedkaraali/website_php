17a638bd2112fa83c2609af12e152d15ebbe8e4c
Majed <majedkaraali@gmail.com>
2023-11-07 13:27:22 +0300

    .

--- index.php
+++ index.php
@@ -7,34 +7,59 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
-    <table border=1>
-        <tr>
-            <td><Label>Adi</Label></td>
-            <td><input  name ="ad" type="text"></td>
-           
-        </tr>
 
-        <tr>
-        <td><Label>Şifre</Label></td>
-        <td><input name= "sifre" type="password"></td>
-        </tr>
-
-        <tr>
-            <td></td>
-            <td ><input type="reset"><input type="submit"></td>
-        </tr>
-
-  </table>
-  </form>
 
   <?php
-  
+  if ($_SERVER["REQUEST_METHOD"]=="POST"){ 
+  echo "merhaba";
+    $ad=$_POST["ad"];
+    $soyad=$_POST["sy"];
+    $sifre=$_POST["sifre"];
+    echo $ad;
+}
+
+    
+
+    else{
+        ?>
+        
+        <form action="index.php" method="post">
+        <table border=1>
+            <tr>
+                <td><Label>Adi</Label></td>
+                <td><input  name ="ad" type="text"></td>
+               
+            </tr>
+    
+            <tr>
+                <td><Label>Soyadi</Label></td>
+                <td><input  name ="sy" type="text"></td>
+               
+            </tr>
+    
+            <tr>
+            <td><Label>Şifre</Label></td>
+            <td><input name= "sifre" type="password"></td>
+            </tr>
+    
+            <tr>
+                <td></td>
+                <td ><input type="reset"><input type="submit"></td>
+            </tr>
+    
+      </table>
+      </form>
+      <?php
+    }
+    ?>
+
+    
+
     
   
   
   
-  ?>
+
 
 
 
