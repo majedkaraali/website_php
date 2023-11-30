3b61f377924c9d993ee75a7b904d6a49288d30fa
Majed <majedkaraali@gmail.com>
2023-11-30 15:00:48 +0300

    update

--- _self/ToDo/index.php
+++ _self/ToDo/index.php
@@ -1,18 +1,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>ToDo</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css_k/styles.css">
+
 </head>
 <body>
-    <div class="header">
-        <div class="nav">
-            <nav></nav>
-        </div>
-        
+    <div class="main">
+        <div class="cal">
+
+
+            <div class="form-cont">
+                <form  method="post">
+            
+                    <label for="email">Email:</label><br>
+                    <input type="email" name="email" id="email"><br>
+                    <label for="pass">Password</label><br>
+                    <input type="password" name="pass" id="pass"><br>
+                    <input type="submit">&nbsp;<input type="reset">
+
+                </form>
+            
+           
+            </div>
+           
+            <div class="right">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut earum minus aspernatur non, sequi odit sint placeat sapiente quasi, ducimus libero nisi facere a soluta neque blanditiis nostrum vel adipisci.</p>
+            </div>
+    </div>
+
     </div>
 </body>
 </html>
