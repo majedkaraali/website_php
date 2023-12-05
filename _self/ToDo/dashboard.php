f19e430a15fc7cbf333c91c6a47fe63e43f85121
Majed <majedkaraali@gmail.com>
2023-12-05 12:07:24 +0300

    update

--- _self/ToDo/dashboard.php
+++ _self/ToDo/dashboard.php
@@ -7,43 +7,62 @@
     <title>Dashboard</title>
 
 </head>
+<h1>Dashboard</h1>
 
 
-<body><h1>Dashboard</h1>
+
+
+<body>
     <div class="menu">
         <h4>welcome</h4>
-
-        <table class="task" border="2px">
-            
-          
-                
-                <tr>
-                    <th>TASK</th>
-                    <th>DATE</th>
-                    <th>STATUS</th>
-                </tr>
-         
-            
-
-            <tr>
-                <td>eat</td>
-                <td>12</td>
-                <td>444</td>
-            </tr>
-
-          
-        </table>
-
     </div>
 
-    <?php
+   
+    <table class="task" border="2px">
+        <thead>             
+            <tr>
+                <th>TASK</th>
+                <th>DATE</th>
+                <th>STATUS</th>
+            </tr>
+        </thead>
+
+     
+
+        <?php
         session_start();
-
-
+        require("data_con.php");
+        
         $user_id = $_SESSION['user_id'];
-        echo"$user_id";
-    ?>
+        $qr="SELECT * FROM todo_items WHERE user_id=$user_id";
+        $qr_run=mysqli_query($conn,$qr);
+        if (!$qr_run) {
+            die("Query failed: " . mysqli_error($conn));
+        }
+
+        if (mysqli_num_rows($qr_run)>0){
+           foreach ($qr_run as $val){
+            
+            ?>
+            <tr>
+            <td><?=$val['task'] ?></td>
+            <td><?=$val['due_date'] ?></td>
+            <td><?=$val['status'] ?></td>
+            </tr>
+
+
+            <?php
+           }
+        }
+
+        ?>
     
+           
     
+
+      
+    </table>
+
+
 </body>
 </html>
