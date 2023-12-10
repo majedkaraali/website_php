2ee8df9174da6d05f5fd4bf32fbf72f008b07480
Majed <majedkaraali@gmail.com>
2023-12-10 22:56:14 +0300

    a

--- _self/ToDo/user/dashboard.php
+++ _self/ToDo/user/dashboard.php
@@ -1,23 +1,17 @@
 <html lang="en">
+    
 <head>
    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"  href="..\res\css\dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <title>Dashboard</title>
-
 
 </head>
 
 
 <body>
-
-
- 
-
-
     <div class="menu">
         <ul>
 
