4ebcf68c5004e2bfe12a4bacfd9607d972980f0c
Majed <47135751+majedkaraali@users.noreply.github.com>
2023-12-13 22:49:36 +0300

    Update index.php

--- _self/ToDo/index.php
+++ _self/ToDo/index.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="res/css/styles.css">
 
 </head>
+
 <body>
 
     <div class="main">
