ec1741a4477716bdd68f4a6d68779bdf5482257e
Majed <majedkaraali@gmail.com>
2023-12-24 18:12:21 +0300

    planned

--- _self/ToDo/php/insert.php
+++ _self/ToDo/php/insert.php
@@ -5,16 +5,21 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $tname = $_POST['tname'];
-    $tdate = $_POST['tdate'];
     $tlist_tag=$_POST['tlist'];
     $user_id = $_SESSION['user_id'];
+    
+    if (isset($_POST['tdate'])){
+        $tdate = $_POST['tdate'];
+        $qr="INSERT INTO common_tasks (task_id,user_id,task_name,status,due_date,list_tag,important,creation_date) VALUES (NULL,'$user_id','$tname','pending','$tdate','$tlist_tag','0', current_timestamp());";
+    }
 
-
-    $qr="INSERT INTO common_tasks (task_id,user_id,task_name,status,list_tag,important,creation_date) VALUES (NULL,'$user_id','$tname','pending','$tlist_tag','0', current_timestamp());";
+    else{
+        $qr="INSERT INTO common_tasks (task_id,user_id,task_name,status,list_tag,important,creation_date) VALUES (NULL,'$user_id','$tname','pending','$tlist_tag','0', current_timestamp());";
+    }
+    
     
     if ($conn->query($qr) === TRUE) {
         echo "Record inserted successfully";
-        echo $tlist_tag;
     } else {
         echo "Error: " . $qr . "<br>" . $conn->error;
     }
