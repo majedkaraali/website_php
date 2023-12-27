f2df909eaa634133b8b89983748f2e33ca3b0eed
Majed <majedkaraali@gmail.com>
2023-12-27 15:49:26 +0300

    clean code

--- _self/ToDo/php/func.php
+++ _self/ToDo/php/func.php
@@ -4,19 +4,6 @@ require("../php/data_con.php");
 $user_id = $_SESSION['user_id'];
 
 
-if (isset($_POST['task_id'])) {
-    updateTaskStatus($_POST['task_id'],$_POST['operation']);
-
-}
-
-
-if (isset( $_POST['listName'])) {
-    create_list( $_POST['listName']);
-}
-
-if (isset( $_POST['del_listName'])) {
-    del_list( $_POST['del_listName']);
-}
 
 
 function create_list($listName){
@@ -61,22 +48,6 @@ function get_record($table,$qr){
 }
 
 
-function displayTasksByList($listType)
-{
-    global $conn;
-    $sql = "SELECT * FROM common_tasks WHERE list_type = '$listType'";
-    $result = $conn->query($sql);
-
-   
-    echo "<h2>$listType Tasks</h2>";
-    echo "<ul>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>{$row['task_name']} - Status: {$row['status']}</li>";
-    }
-    echo "</ul>";
-
-    $conn->close();
-}
 
 
 function updateTaskStatus($task_id,$operation){
@@ -143,6 +114,37 @@ function getMissingTasksCount() {
     return $count;
 }
 
+
+function displayTasksByList($listType)
+{
+    global $conn;
+    $sql = "SELECT * FROM common_tasks WHERE list_type = '$listType'";
+    $result = $conn->query($sql);
+
+   
+    echo "<h2>$listType Tasks</h2>";
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>{$row['task_name']} - Status: {$row['status']}</li>";
+    }
+    echo "</ul>";
+
+    $conn->close();
+}
+
+if (isset($_POST['task_id'])) {
+    updateTaskStatus($_POST['task_id'],$_POST['operation']);
+
+}
+
+
+if (isset( $_POST['listName'])) {
+    create_list( $_POST['listName']);
+}
+
+if (isset( $_POST['del_listName'])) {
+    del_list( $_POST['del_listName']);
+}
 
 ?>
 
